0e93a628666e7b332522933f858d0d374ea8b5b4
Kyohei SASAKI <athene609.cyl@gmail.com>
2018-02-02 12:45:44 +0900

    なぜかうまくいったやつです
    わからん（わからん）

--- src/replacer/replacer.ts
+++ src/replacer/replacer.ts
@@ -1,56 +1,68 @@
-const init = () => {
+const init = function () {
 	const body = document.getElementById('content')!;
-	const caret = '<span class="caret"></span>';
-	const typing = async function (strs: string, timing = 80) {
-		const push = (char: string, _target: HTMLElement) => {
-			// target.innerHTML = stdout + char + caret;
-			console.log(stdout + char + caret);
-			return char;
-		};
-		let stdout = '';
-		const loop = function (i: number) {
-			return (function () {
-				setTimeout(() => {
-					stdout += push(strs[i], body);
-					loop(i + 1);
-				}, timing);
-			});
-		}
-		loop(0);
-		const type = async function (strs: string) {
-			return Promise.resolve(0)
-				.then(function loop(i) {
-					return new Promise(function (resolve) {
-						if(i < strs.length) {
-							setTimeout(function () {
-								stdout += push(strs[i], body);
-								resolve(i + 1);
-							}, timing);
-						} else {
-							setTimeout(function () {
-								push('<br>', body);
-								return 1;
-							}, timing * 3);
-						}
-					})
-					.then(<any>loop);
-				});
-		};
+	const p = document.createElement('p');
+	p.classList.add('caret');
+
+	const wait = function (time: number) {
+		return new Promise(function (resolve) {
+			const span = document.createElement('span');
+			p.appendChild(span);
+			span.innerHTML += '<br>';
+			setTimeout(resolve, time);
+		});
 	};
 
+	const typing = function (strs: string, timing = 90) {
+		let i = 0;
+		const span = document.createElement('span');
+		p.appendChild(span);
 
-	body.innerHTML = caret;
-	typing('これをあなたが読んでいる時、');
-	// async function sample() {
-	// 	await typing('これをあなたが読んでいる時、');
-	// 	await typing('わたしはわたしではないだろう。')
-	// }
-	// sample();
-		// .then(()=>typing('', 1200))
-		// .then(()=>typing('このメッセージが表示されたということは、'))
-		// .then(()=>typing('そこにはあなた、わたし、涼宮ハルヒ、朝比奈みくる、'))
-		// .then(()=>typing('古泉一樹が存在しているはずである。'));
+		return new Promise(function (resolve) {
+			let a: any = setInterval((function () {
+				if (i < strs.length) {
+					span.innerHTML += strs[i++];
+				} else {
+					span.innerHTML += '<br>';
+					clearInterval(a);
+					setTimeout(resolve, timing * 8);
+				}
+			}), timing);
+		});
+	};
 
+	body.innerHTML = '';
+	body.appendChild(p);
+	typing('これをあなたが読んでいる時、')
+		.then(() => typing('わたしはわたしではないだろう。'))
+		.then(() => wait(4000))
+		.then(() => typing('このメッセージが表示されたということは、'))
+		.then(() => typing('そこにはあなた、わたし、涼宮ハルヒ、朝比奈みくる、'))
+		.then(() => typing('古泉一樹が存在しているはずである。'))
+		.then(() => wait(4000))
+		.then(() => typing('それが鍵。'))
+		.then(() => typing('あなたは解答を見つけ出した。'))
+		.then(() => wait(1000))
+		.then(() => wait(1000))
+		.then(() => wait(1000))
+		.then(() => wait(1000))
+		.then(() => typing('これは緊急脱出プログラムである。'))
+		.then(() => typing(''))
+		.then(() => typing('起動させる場合はエンターキーを、'))
+		.then(() => typing('そうでない場合はそれ以外のキーを選択せよ。'))
+		.then(() => wait(2000))
+		.then(() => typing('起動させた場合、'))
+		.then(() => typing('あなたは時空修正の機会を得る。'))
+		.then(() => typing('ただし成功は保障できない。'))
+		.then(() => typing('また帰還の保障もできない。'))
+		.then(() => wait(2000))
+		.then(() => typing('このプログラムが起動するのは一度きりである。'))
+		.then(() => typing('実行ののち、消去される。'))
+		.then(() => typing(''))
+		.then(() => typing('非実行が選択された場合は起動せずに消去される。'))
+		.then(() => wait(1000))
+		.then(() => wait(1000))
+		.then(() => wait(2000))
+		.then(() => typing('Ｒｅａｄｙ？', 700))
 
 };
 
--- ~/replacer/replacer.js
+++ ~/replacer/replacer.js
@@ -70,36 +70,68 @@
 
 "use strict";
 
-const init = () => {
-    // if (location.pathname !==
-    // 	'/page/%E3%83%A1%E3%82%A4%E3%83%B3%E3%83%9A%E3%83%BC%E3%82%B8')
-    // 	return;
+const init = function () {
     const body = document.getElementById('content');
-    const caret = '<span class="caret"></span>';
-    const sleep = async function (delay) {
-        return new Promise(resolve => {
-            setTimeout(resolve, delay);
+    const p = document.createElement('p');
+    p.classList.add('caret');
+    const wait = function (time) {
+        return new Promise(function (resolve) {
+            const span = document.createElement('span');
+            p.appendChild(span);
+            span.innerHTML += '<br>';
+            setTimeout(resolve, time);
         });
     };
-    const typing = async function (strs, timing = 80) {
-        const chars = strs.split('');
-        const pushChar = (char, target) => {
-            target.innerHTML += char + caret;
-        };
-        Promise.resolve(0).then(function loop(i) {
-            return new Promise(function (resolve) {
-                if (i < chars.length) {
-                    setTimeout(function () {
-                        pushChar(chars[i], body);
-                        resolve(i + 1);
-                    }, timing);
+    const typing = function (strs, timing = 90) {
+        let i = 0;
+        const span = document.createElement('span');
+        p.appendChild(span);
+        return new Promise(function (resolve) {
+            let a = setInterval((function () {
+                if (i < strs.length) {
+                    span.innerHTML += strs[i++];
                 }
-            })
-                .then(loop);
+                else {
+                    span.innerHTML += '<br>';
+                    clearInterval(a);
+                    setTimeout(resolve, timing * 8);
+                }
+            }), timing);
         });
     };
-    body.innerHTML = caret;
-    typing('あいうえお　かきくけこ');
+    body.innerHTML = '';
+    body.appendChild(p);
+    typing('これをあなたが読んでいる時、')
+        .then(() => typing('わたしはわたしではないだろう。'))
+        .then(() => wait(4000))
+        .then(() => typing('このメッセージが表示されたということは、'))
+        .then(() => typing('そこにはあなた、わたし、涼宮ハルヒ、朝比奈みくる、'))
+        .then(() => typing('古泉一樹が存在しているはずである。'))
+        .then(() => wait(4000))
+        .then(() => typing('それが鍵。'))
+        .then(() => typing('あなたは解答を見つけ出した。'))
+        .then(() => wait(1000))
+        .then(() => wait(1000))
+        .then(() => wait(1000))
+        .then(() => wait(1000))
+        .then(() => typing('これは緊急脱出プログラムである。'))
+        .then(() => typing(''))
+        .then(() => typing('起動させる場合はエンターキーを、'))
+        .then(() => typing('そうでない場合はそれ以外のキーを選択せよ。'))
+        .then(() => wait(2000))
+        .then(() => typing('起動させた場合、'))
+        .then(() => typing('あなたは時空修正の機会を得る。'))
+        .then(() => typing('ただし成功は保障できない。'))
+        .then(() => typing('また帰還の保障もできない。'))
+        .then(() => wait(2000))
+        .then(() => typing('このプログラムが起動するのは一度きりである。'))
+        .then(() => typing('実行ののち、消去される。'))
+        .then(() => typing(''))
+        .then(() => typing('非実行が選択された場合は起動せずに消去される。'))
+        .then(() => wait(1000))
+        .then(() => wait(1000))
+        .then(() => wait(2000))
+        .then(() => typing('Ｒｅａｄｙ？', 700));
 };
 if (document.readyState !== 'loading') {
     init();
